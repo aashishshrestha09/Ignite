7df75a0a222103bf4051259f2942a162df6562d2
aashishshrestha09 <54453596+aashishshrestha09@users.noreply.github.com>
2020-12-12 04:25:45 -0500

    3. Setting Up Redux

--- src/index.js
+++ src/index.js
@@ -1,5 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+// Redux setup
+import {createStore} from 'redux';
+import rootReducer from './reducers'
+import {Provider} from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(
+    rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App /> 
+    </Provider>, 
+    document.getElementById('root')
+);
